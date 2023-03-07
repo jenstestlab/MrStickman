61ed5852ddee2b9841ac1999d3e8dd7e4f2d1711
Jenny <112797418+jenstestlab@users.noreply.github.com>
2023-03-07 12:21:10 +0100

    Update MrStcikman.py

--- MrStcikman.py
+++ MrStcikman.py
@@ -1,36 +1,35 @@
 from tkinter import *
 import random
 import time
+
 class Spel:
     def __init__(self):
         self.tk = Tk()
-        self.tk.title("Mr Stick man holt naar de uitgang")
-        self.tk.resizable(0,0)
-        self.tk.wm_attributes('-topmost',1)
-        self.canvas = Canvas(self.tk, width=500, height = 500, highlightthickness=0)
+        self.tk.title("Mr. Stick Man holt naar de uitgang")
+        self.tk.resizable(0, 0)
+        self.tk.wm_attributes("-topmost", 1)
+        self.canvas = Canvas(self.tk, width=500, height=500, highlightthickness=0)
         self.canvas.pack()
         self.tk.update()
         self.canvas_height = 500
         self.canvas_width = 500
-
-        self.bg = PhotoImage(file = 'achtergrond.gif')
-        w= self.bg.width()
-        h= self.bg.height()
-        for x in range (0,5):
-            for y in range (0,5):
-                self.canvas.create_image(x * w, y * h, image= self.bg ,anchor = 'nw')
-        self.sprites= []
+        self.bg = PhotoImage(file="achtergrond.gif")
+        w = self.bg.width()
+        h = self.bg.height()
+        for x in range(0, 5):
+            for y in range(0, 5):
+                self.canvas.create_image(x * w, y * h, image=self.bg, anchor='nw')
+        self.sprites = []
         self.running = True
 
     def hoofdlus(self):
         while 1:
             if self.running == True:
-                for sprite in  self.sprites:
+                for sprite in self.sprites:
                     sprite.move()
             self.tk.update_idletasks()
             self.tk.update()
             time.sleep(0.01)
-
 
 class Coords:
     def __init__(self, x1=0, y1=0, x2=0, y2=0):
@@ -51,46 +50,51 @@ class Coords:
 #     else:
 #         return False
 
-def within_x (co1,co2):
-    if (co1.x1 > co2.x1 and co1.x1 <co2.x2) or  (co1.x2 > co2.x1 and co1.x2 <co2.x2) or (co2.x1 > co1.x1 and co2.x1 <co1.x2) or (co2.x2 > co1.x1 and co2.x2 <co1.x2):
+def within_x(co1, co2):
+    if (co1.x1 > co2.x1 and co1.x1 < co2.x2) \
+            or (co1.x2 > co2.x1 and co1.x2 < co2.x2) \
+            or (co2.x1 > co1.x1 and co2.x1 < co1.x2) \
+            or (co2.x2 > co1.x1 and co2.x2 < co1.x2):
         return True
     else:
         return False
 
-def within_y (co1,co2):
-    if (co1.y1 > co2.y1 and co1.y1 <co2.y2) or  (co1.y2 > co2.y1 and co1.y2 <co2.y2) or (co2.y1 > co1.y1 and co2.y1 <co1.y2) or (co2.y2 > co1.y1 and co2.y2 <co1.y2):
+def within_y(co1, co2):
+    if (co1.y1 > co2.y1 and co1.y1 < co2.y2) \
+            or (co1.y2 > co2.y1 and co1.y2 < co2.y2) \
+            or (co2.y1 > co1.y1 and co2.y1 < co1.y2) \
+            or (co2.y2 > co1.y1 and co2.y2 < co1.y2):
         return True
     else:
         return False
 
-def collided_left(co1,co2):
-    if within_y (co1,co2):
+def collided_left(co1, co2):
+    if within_y(co1, co2):
         if co1.x1 <= co2.x2 and co1.x1 >= co2.x1:
             return True
     return False
 
-def collided_right(co1,co2):
-    if within_y (co1,co2):
-        if co1.x2 <= co2.x1 and co1.x2 <= co2.x2:
+def collided_right(co1, co2):
+    if within_y(co1, co2):
+        if co1.x2 >= co2.x1 and co1.x2 <= co2.x2:
             return True
     return False
 
-def collided_top(co1,co2):
-    if within_x (co1,co2):
+def collided_top(co1, co2):
+    if within_x(co1, co2):
         if co1.y1 <= co2.y2 and co1.y1 >= co2.y1:
             return True
     return False
 
-def collided_bottom(y,co1,co2):
-    if within_y (co1,co2):
+def collided_bottom(y, co1, co2):
+    if within_x(co1, co2):
         y_calc = co1.y2 + y
-        if y_calc>= co2.y1 and y_calc <= co2.y2:
+        if y_calc >= co2.y1 and y_calc <= co2.y2:
             return True
     return False
 
-
 class Sprite:
-    def __init__(self,game):
+    def __init__(self, game):
         self.game = game
         self.endgame = False
         self.coordinates = None
@@ -100,39 +104,38 @@ class Sprite:
         return self.coordinates
 
 class PlatformSprite(Sprite):
-    def __init__(self, game, photo_image, x,y, width, height):
+    def __init__(self, game, photo_image, x, y, width, height):
         Sprite.__init__(self, game)
         self.photo_image = photo_image
-        self.image = game.canvas.create_image(x,y, image=self.photo_image,anchor = "nw")
-        self.coordinates = Coords(x,y, x + width, y + height)
+        self.image = game.canvas.create_image(x, y, image=self.photo_image, anchor='nw')
+        self.coordinates = Coords(x, y, x + width, y + height)
 
 class StickFigureSprite(Sprite):
     def __init__(self, game):
-        Sprite.__init__(self,game)
+        Sprite.__init__(self, game)
         self.images_left = [PhotoImage(file = 'L1.gif'),PhotoImage(file = 'L2.gif'),PhotoImage(file = 'L3.gif')]
         self.images_right = [PhotoImage(file = 'R1.gif'),PhotoImage(file = 'R2.gif'),PhotoImage(file = 'R3.gif')]
-        self.image = game.canvas.create_image(200,470, image=self.images_left[0], anchor = 'nw')
+        self.image = game.canvas.create_image(200, 470, image=self.images_left[0], anchor='nw')
         self.x = -2
-        self.y =0
+        self.y = 0
         self.current_image = 0
         self.current_image_add = 1
         self.jump_count = 0
         self.last_time = time.time()
         self.coordinates = Coords()
+        game.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        game.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+        game.canvas.bind_all('<space>', self.jump)
 
-        game.canvas.bind_all('<KeyPress-Left>',self.turn_left)
-        game.canvas.bind_all('<KeyPress-Right>',self.turn_right)
-        game.canvas.bind_all('<KeyPress-Up>',self.jump)
-
-    def turn_left(self,evt):
+    def turn_left(self, evt):
         if self.y == 0:
             self.x = -2
 
-    def turn_right(self,evt):
+    def turn_right(self, evt):
         if self.y == 0:
             self.x = 2
 
-    def jump(self,evt):
+    def jump(self, evt):
         if self.y == 0:
             self.y = -4
             self.jump_count = 0
@@ -148,19 +151,18 @@ class StickFigureSprite(Sprite):
                     self.current_image_add = 1
         if self.x < 0:
             if self.y != 0:
-                self.game.canvas.itemconfig(self.image, image= self.images_left [2])
-
+                self.game.canvas.itemconfig(self.image, image=self.images_left[2])
             else:
-                self.game.canvas.itemconfig(self.image, image= self.images_left[self.current_image])
+                self.game.canvas.itemconfig(self.image, image=self.images_left[self.current_image])
         elif self.x > 0:
             if self.y != 0:
-                self.game.canvas.itemconfig(self.image, image=self.images_right [2])
+                self.game.canvas.itemconfig(self.image, image=self.images_right[2])
             else:
                 self.game.canvas.itemconfig(self.image, image=self.images_right[self.current_image])
 
     def coords(self):
         xy = list(self.game.canvas.coords(self.image))
-        self.coordinates.x1 = xy[0] 
+        self.coordinates.x1 = xy[0]
         self.coordinates.y1 = xy[1]
         self.coordinates.x2 = xy[0] + 27
         self.coordinates.y2 = xy[1] + 30
@@ -170,20 +172,20 @@ class StickFigureSprite(Sprite):
         self.animate()
         if self.y < 0:
             self.jump_count += 1
-            if self.jump_count >20:
+            if self.jump_count > 20:
                 self.y = 4
-        if self.y >0:
+        if self.y > 0:
             self.jump_count -= 1
         co = self.coords()
         left = True
         right = True
         top = True
-        bottom =  True
-        falling  = True
+        bottom = True
+        falling = True
         if self.y > 0 and co.y2 >= self.game.canvas_height:
             self.y = 0
             bottom = False
-        elif  self.y < 0  and co.y1 <= 0 :
+        elif self.y < 0 and co.y1 <= 0:
             self.y = 0
             top = False
         if self.x > 0 and co.x2 >= self.game.canvas_width:
@@ -199,28 +201,29 @@ class StickFigureSprite(Sprite):
             if top and self.y < 0 and collided_top(co, sprite_co):
                 self.y = -self.y
                 top = False
-            if bottom and self.y > 0 and collided_bottom(self.y,  co, sprite_co):
+            if bottom and self.y > 0 and collided_bottom(self.y, co, sprite_co):
                 self.y = sprite_co.y1 - co.y2
-                if self.y <0:
+                if self.y < 0:
                     self.y = 0
                 bottom = False
                 top = False
-            if bottom and falling and self.y == 0 and co.y2 <self.game.canvas_height and collided_bottom(1, co,sprite_co):
+
+            if bottom and falling and self.y == 0 and co.y2 < self.game.canvas_height and collided_bottom(1, co, sprite_co):
                 falling = False
 
             if left and self.x < 0 and collided_left(co, sprite_co):
                 self.x = 0
-                left =  False
+                left = False
                 if sprite.endgame:
-                    self.game.running  = False
+                    self.game.running = False
             if right and self.x > 0 and collided_right(co, sprite_co):
                 self.x = 0
                 right = False
                 if sprite.endgame:
                     self.game.running = False
         if falling and bottom and self.y == 0 and co.y2 < self.game.canvas_height:
-            self.y =4
-            self.game.canvas.move(self.image, self.x, self.y)
+            self.y = 4
+        self.game.canvas.move(self.image, self.x, self.y)
     
 
     
@@ -228,8 +231,8 @@ class  DoorSprite(Sprite):
     def __init__(self, game, photo_image, x,y,width, height):
         Sprite.__init__(self, game)
         self.photo_image = photo_image
-        self.image = game.canvas.create_image(x,y,image= self.photo_image, anchor = 'nw')
-        self.coordinates = Coords(x,y,x+(width/2),y+ height)
+        self.image = game.canvas.create_image(x, y,image= self.photo_image, anchor = 'nw')
+        self.coordinates = Coords(x, y, x + (width / 2), y + height)
         self.endgame = True
 
     
@@ -238,7 +241,6 @@ class  DoorSprite(Sprite):
 
 g = Spel()
 platform1 = PlatformSprite(g,PhotoImage(file = 'platform1.gif'), 0,480,100,10)
-
 platform2 = PlatformSprite(g,PhotoImage(file = 'platform1.gif'), 150,440,100,10)
 platform3 = PlatformSprite(g,PhotoImage(file = 'platform1.gif'), 300,400,100,10)
 platform4 = PlatformSprite(g,PhotoImage(file = 'platform1.gif'), 300,160,100,10)
